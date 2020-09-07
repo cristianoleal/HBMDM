df1f44044d4744d8b74380054cd117c2265fe3d0
Cristiano Leal <cristiano.leal@hbsis.com.br>
2020-09-07 17:35:04 -0300

    ajuste arquivo env para rodar a pipeline no modo headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,22 +11,13 @@ when "firefox"
 when "chrome"
   @driver = :selenium_chrome
 when "headless"
-  Capybara.register_driver :selenium_chrome_headless do |app|
-    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
-      options.add_arguments "--headless"
-      options.add_arguments "--disable-gpu"
-      options.add_arguments "--no-sandbox"
-      options.add_arguments "--disable-site-isolation-trials"
-    end
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
-  end
   @driver = :selenium_chrome_headless
 else
   log "Browser inválido"
 end
 
-Capybara.configure do |config|
-  config.default_driver = @driver
-  config.app_host = CONFIG["url"]
-  config.default_max_wait_time = 10
+Capybara.configure do |c|
+  c.default_driver = @driver
+  c.app_host = CONFIG["url"]
+  c.default_max_wait_time = 10
 end
